a1f7f46bdd144a6996a9cc136a09e309f23b2767
Prashankhan Chandrapalan <prashankhan63@gmail.com>
2021-09-04 01:51:41 +0530

    commited

--- components/HomeCallToAction.js
+++ components/HomeCallToAction.js
@@ -1,23 +1,30 @@
-import Link from "next/link";
+/** @format */
 
+import Link from 'next/link';
+import HomeCallToActionData from './../data/HomeCallToActionData';
 
 const HomeCallToAction = () => {
-    return(
-        <div className='container home-call-to-action-container p-5 mb-5'>
-            <div className='row'>
-                <div className='col-md-10'>
-                <h1 className='text-white mt-4'>Have an idea to build a digital product?</h1>
-                <p className='text-white '>build a digital product an idea to build a digital product an idea to
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer...
-                </p>
-                </div>
-                <div className='col-md-2 d-grid align-items-center'>
-                <Link href='/pricing' ><a className="button  large fourth " style={{marginLeft:'5px', marginTop:'10px'}}>Get a Quote</a></Link>
-            
-                </div>
-                </div>
-        </div>
-    )
-}
+	return (
+		<div className='container home-call-to-action-container mb-5'>
+			<div className='row justify-content-md-center home-call-to-action-container-row'>
+				<div className='col-lg-7 d-flex align-items-center'>
+					{HomeCallToActionData.map((content, i) => {
+						return (
+							<div key={i} className='text-center'>
+								<h1 className='text-white mt-4'>{content.title}</h1>
+								<p className='text-white '> {content.excerpt}</p>
+								<Link href={content.ButtonLink}>
+									<a className='button  large fourth ' style={{ marginLeft: '5px', marginTop: '10px' }}>
+										{content.ButtonText}
+									</a>
+								</Link>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default HomeCallToAction
+export default HomeCallToAction;
---
+++ data/HomeCallToActionData.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const HomeCallToActionData = [
+	{
+		id: '1',
+		title: 'Have an idea for build a digital products?',
+		excerpt: 'lorem ipsum dolor sit amet, consectet, lorem, sed diam nonumy eirmod tempor incididunt ut labore et d',
+		ButtonLink: '/get-quote',
+		ButtonText: 'Get a Quote',
+	},
+];
+
+export default HomeCallToActionData;
