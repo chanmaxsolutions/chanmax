46e5b3b7ac1fa5be7c34decdf0155e8dd07bdc36
raghuraj20 <balasubramaniam.raghuraj@gmail.com>
2021-09-07 22:27:05 +0530

    commit

--- components/Toast.js
+++ components/Toast.js
@@ -3,24 +3,21 @@ import React from "react";
 export default function Toast({ message, handleClose }) {
     return (
         <div
-            style={{
-                position: "fixed",
-                padding: 20,
-                top: "calc(100vh - 100px)",
-                left: 20,
-                width: 300,
-                border: "1px solid black",
-                background: "white",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-            }}
+            className="toast align-items-center text-white bg-primary border-0"
+            style={{ top: "calc(100vh - 70px)", position: "fixed", left: 10, opacity: 1 }}
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
         >
-            <div>{message}</div>
-            <div>
-                <button type="button" className="btn-close ms-2" onClick={handleClose}>
-                    <span className="visually-hidden">Close</span>
-                </button>
+            <div className="d-flex">
+                <div className="toast-body">{message}</div>
+                <button
+                    type="button"
+                    className="btn-close btn-close-white me-2 m-auto"
+                    data-bs-dismiss="toast"
+                    aria-label="Close"
+                    onClick={handleClose}
+                ></button>
             </div>
         </div>
     );
