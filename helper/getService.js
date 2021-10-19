4ab6d3a83b65e88e1118d2c2f36ec3612a46cf65
Prashankhan Chandrapalan <prashankhan63@gmail.com>
2021-10-19 23:38:11 +0530

    updated

--- helper/getService.js
+++ helper/getService.js
@@ -1,15 +1,18 @@
-const api_url = "https://chanmax-opening.herokuapp.com/api";
+/** @format */
+
+// const api_url = "https://chanmax-opening.herokuapp.com/api";
+const api_url = '/';
 
 export const getService = (api) => {
-    return fetch(api_url + api, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .catch((err) => console.log(err));
+	return fetch(api_url + api, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((err) => console.log(err));
 };
--- helper/postService.js
+++ helper/postService.js
@@ -1,16 +1,19 @@
-const api_url = "https://chanmax-opening.herokuapp.com/api";
+/** @format */
+
+// const api_url = "https://chanmax-opening.herokuapp.com/api";
+const api_url = '/';
 
 export const postService = (api, data) => {
-    return fetch(api_url + api, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .catch((err) => console.log(err));
+	return fetch(api_url + api, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((err) => console.log(err));
 };
