65ae6350e0d77934590439e47f51a14e5ea9cb9e
Prashankhan Chandrapalan <prashankhan63@gmail.com>
2021-10-20 21:52:28 +0530

    updated

--- helper/getService.js
+++ helper/getService.js
@@ -1,17 +1,17 @@
 /** @format */
 
-const api_url = 'https://chanmax-opening.herokuapp.com/api';
+// const api_url = 'https://chanmax-opening.herokuapp.com/api';
 
-export const getService = (api) => {
-	return fetch(api_url + api, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-		},
-	})
-		.then((response) => {
-			return response.json();
-		})
-		.catch((err) => console.log(err));
-};
+// export const getService = (api) => {
+// 	return fetch(api_url + api, {
+// 		method: 'GET',
+// 		headers: {
+// 			Accept: 'application/json',
+// 			'Content-Type': 'application/json',
+// 		},
+// 	})
+// 		.then((response) => {
+// 			return response.json();
+// 		})
+// 		.catch((err) => console.log(err));
+// };
