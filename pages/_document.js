6c803622ed37db6ee3f20aa460c036cb4e84b71f
Prashankhan Chandrapalan <prashankhan63@gmail.com>
2021-11-11 01:29:19 +0530

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ class MyDocument extends Document {
 				</Head>
 				<body>
 					<Main />
-					{/* <script src={'/js/plugins.js'} /> */}
+					<script src={'/js/plugins.js'} />
 					{/* <script
 						src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
 						integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4'
