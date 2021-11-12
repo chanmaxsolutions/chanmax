dd53cf22388d9a7b9895c0af741ba247fe1801b2
Prashankhan Chandrapalan <prashankhan63@gmail.com>
2021-11-12 23:18:59 +0530

    updated

--- components/SEO.js
+++ components/SEO.js
@@ -11,15 +11,24 @@ const SEO = ({ title, desc, img }) => {
 			<meta name='google' content='notranslate' />
 			<meta
 				name='description'
-				content='We are Building lightning-fast Applications from Startups to Corporates.'
+				content='We are a vigilant team of digital experts ever ready to validate 
+				your vision towards groundbreaking success, developing purposeful novel 
+				solutions with high-end technologies. We architect future proof applications 
+				to create game-changing experiences that make you unique and the first choice in the market.'
 			/>
 			<link
 				rel='icon'
 				href='https://res.cloudinary.com/dem8lqxhj/image/upload/v1636590585/Home/favicon_gbzjjm.png'
 			/>
-			<meta property="og:title" content={title} />
-			<meta property="og:description" content={desc || title} />
-			<meta property="og:image" content={img || "https://res.cloudinary.com/dem8lqxhj/image/upload/v1636620911/Home/logo_6_jxem7d.jpg"} />
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={desc || title} />
+			<meta
+				property='og:image'
+				content={
+					img ||
+					'https://res.cloudinary.com/dem8lqxhj/image/upload/v1636620911/Home/logo_6_jxem7d.jpg'
+				}
+			/>
 		</Head>
 	);
 };
