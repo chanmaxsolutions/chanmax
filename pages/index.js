9e82d703c95a70daca1e96e2dc8d0c36005071e4
Prashankhan Chandrapalan <prashankhan63@gmail.com>
2021-11-14 23:39:09 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import TestimonialCarousel from '../components/TestimonialCarousel';
 export default function Home() {
 	return (
 		<div>
-			<SEO title='CHANMAX TECHNOLOGIES | BUILDING APPS FOR STARTUPS TO CORPORATES' />
+			<SEO title='CHANMAX | BUILDING APPS FOR STARTUPS TO CORPORATES' />
 			<HomeHero />
 			<ClientCarousel />
 			<HomeWhySection />
